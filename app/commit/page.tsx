e33d4b3cc324395009a34f0d94b9ce5fc96a2937
jhfnetboy <jhfnetboy@gmail.com>
2025-01-06 12:45:27 +0700

    fix config

--- app/commit/page.tsx
+++ app/commit/page.tsx
@@ -1,1 +1,18 @@
- 
+'use client';
+
+import CommitPointsForm from '@/components/commit-points-form';
+import { useRouter } from 'next/navigation';
+
+export default function CommitPage() {
+  const router = useRouter();
+  
+  const handleBack = () => {
+    router.push('/');
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <CommitPointsForm onBack={handleBack} />
+    </div>
+  );
+} 
